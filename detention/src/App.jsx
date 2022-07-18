e43cc4d3fae407d99fd40fc797d9b5a97b320570
MHeinz1996 <michael.heinzinger@gmail.com>
2022-07-18 16:55:36 -0500

    I think I did what the assignment wanted

---
+++ detention/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+import Sentences from './components/Sentences'
+import DisplaySentences from './components/DisplaySentences'
+
+function App() {
+  const [sentence, setSentence] = useState('')
+
+  function writeSentence() {
+    setSentence('I will never mutate state or props directly')
+    console.log(sentence)
+  }
+
+  return (
+    <div>
+      <h1>Detention!</h1>
+      <Sentences handleClick={writeSentence}/>
+      <ul className='sentences'>
+        <DisplaySentences lines={sentence} />
+      </ul>
+    </div>
+  )
+}
+
+export default App
